ee1baf6194b11f96e91689950a27f74a3c2319f3
Felix A. Schultz <felix.schultz@engapho.com>
2024-02-22 20:01:40 +0100

    Add user schema and password hashing hook

--- app/db/models.js
+++ app/db/models.js
@@ -1,4 +1,5 @@
 import { mongoose } from "mongoose";
+import bcrypt from "bcrypt";
 
 const { Schema } = mongoose;
 
@@ -23,6 +24,41 @@ const entrySchema = new Schema(
   { timestamps: true },
 );
 
+// User Schema
+const userSchema = new mongoose.Schema(
+  {
+    image: String,
+    mail: {
+      type: String,
+      required: true, // Ensure user emails are required
+      unique: true // Ensure user emails are unique
+    },
+    name: String,
+    title: String,
+    educations: [String],
+    password: {
+      type: String,
+      required: true, // Ensure user passwords are required
+      select: false // Automatically exclude from query results
+    }
+  },
+  { timestamps: true }
+);
+
+// pre save password hook
+userSchema.pre("save", async function (next) {
+  const user = this; // this refers to the user document
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified("password")) {
+    return next(); // continue
+  }
+
+  const salt = await bcrypt.genSalt(10); // generate a salt
+  user.password = await bcrypt.hash(user.password, salt); // hash the password
+  next(); // continue
+});
+
 // For each model you want to create, please define the model's name, the
 // associated schema (defined above), and the name of the associated collection
 // in the database (which will be created automatically).
@@ -32,4 +68,9 @@ export const models = [
     schema: entrySchema,
     collection: "entries",
   },
+  {
+    name: "User",
+    schema: userSchema,
+    collection: "users"
+  }
 ];
