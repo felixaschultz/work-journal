57053056f047f8ec0d079710fe2a5f2f8c279f81
Felix A. Schultz <felix.schultz@engapho.com>
2024-02-22 21:46:39 +0100

    Added cookie secrets, sameSite, path, maxAge and httpOnly attributes

--- app/services/session.jsx
+++ app/services/session.jsx
@@ -3,5 +3,10 @@ import { createCookieSessionStorage } from "@remix-run/node";
 export const { getSession, commitSession, destroySession } = createCookieSessionStorage({
     cookie: {
         name: "work-journal-session",
+        secrets: ["build-ui-secret"],
+        sameSite: "lax",
+        path: "/",
+        maxAge: 60 * 60 * 24 * 30,
+        httpOnly: true,
     },
 })
