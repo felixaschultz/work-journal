71730c4ebcfe5b9e5ca26b6553488d2c8c3d357e
Felix A. Schultz <felix.schultz@engapho.com>
2024-02-23 11:09:17 +0100

    Add error message for invalid login

--- app/routes/login.jsx
+++ app/routes/login.jsx
@@ -2,9 +2,12 @@ import { Form, useLoaderData, Link, useActionData} from "@remix-run/react";
 import { commitSession, getSession } from "~/services/session";
 import bcrypt from "bcrypt";
 import mongoose from "mongoose";
-import { redirect } from "@remix-run/node";
+import { redirect, json } from "@remix-run/node";
+import { is } from "date-fns/locale";
 
 export default function Login(){
+    let data = useLoaderData();
+    let actionData = useActionData();
     return (
         <>
             <div className="grid place-content-center place-items-center h-screen p-8 text-slate-50 bg-slate-900">
@@ -20,6 +23,9 @@ export default function Login(){
                             <input className="w-full text-slate-500 p-2" type="password" id="password" name="password" required />
                         </div>
                         <button className="rounded-md w-full bg-slate-500 p-2 mt-2 disabled:bg-slate-50 items-end" type="submit">Login</button>
+                        {actionData === "Bad credentials" && (
+                            <p className="mt-4 font-medium text-red-500">Invalid login.</p>
+                        )}
                     </Form>
                 </section>
             </div>
@@ -35,17 +41,25 @@ export async function action({ request }) {
     const user = await mongoose.models.User.findOne({ email }).select("+password");
     /* const isPasswordValid = await bcrypt.compare(password, user.password); */
     user.password = undefined;
+    let error;
 
-    if(user){
+    /* if(!isPasswordValid){
+        error = "Bad credentials";
+    }else{
+        error = null;
+    } */
+
+    if(user && !error){
         let session = await getSession();
         session.set("isAdmin", true);
         await commitSession(session);
+
         return redirect("/", {
             headers: {
               "Set-Cookie": await commitSession(session)
             },
         });
     }else{
-        return "Unauthorized";
+        return json({ error: error }, 401);
     }
 }
