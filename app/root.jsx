9d147ecb04084f4e181950ce24f2ce3836a2ee2c
Felix A. Schultz <felix.schultz@engapho.com>
2024-02-23 09:20:39 +0100

    Add session management and login/logout functionality

--- app/root.jsx
+++ app/root.jsx
@@ -8,6 +8,8 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import styles from "./tailwind.css";
+import { getSession } from "~/services/session";
+import { useLoaderData } from "@remix-run/react";
 
 export const links = () => [
   {
@@ -20,8 +22,13 @@ export function meta() {
   return [{ title: "Work Journal" }];
 }
 
+export async function loader({ request }) {
+  const user = await getSession(request.headers.get("Cookie"));
+  return { session: user.data };
+}
+
 export default function App() {
-  
+  const { session } = useLoaderData();
   return (
     <html lang="en">
       <head>
@@ -30,7 +37,18 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="bg-slate-900">
+        <header className="grid grid-cols-2 p-4">
+          <h1 className="text-3xl text-slate-50 font-bold">Weekly Journal</h1>
+          {
+            !session.isAdmin && 
+            <Link to="/login" className="block min-w-max ml-auto w-fit py-2 px-11 text-slate-100 bg-slate-500 rounded-md">Login</Link>
+          }
+          {
+            session.isAdmin && 
+            <Link to="/logout" className="block min-w-max ml-auto w-fit py-2 px-11 text-slate-100 bg-slate-500 rounded-md">Logout</Link>
+          }
+        </header>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
